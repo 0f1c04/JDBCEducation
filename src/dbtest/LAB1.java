d46f5f8e661afc456e28a0b3d20df5777f117c5f
0f1c04 <f1c04@kakao.com>
2021-03-11 17:50:09 +0900

    #Day18 TIL: JDBC interlock, Study SQL function

---
+++ src/dbtest/LAB1.java
@@ -0,0 +1,41 @@
+package dbtest;
+import util.DBUtil;
+
+import java.sql.*;
+import java.util.Date;
+import java.util.Scanner;
+//[LAB] 급여를 가변으로 입력받아 입력값 이상의 급여를 받는 직원들 조회
+//직원이름, 급여, 입사일, 부서번호를 출력한다.
+public class LAB1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String sql = "select LAST_NAME, SALARY, HIRE_DATE, DEPARTMENT_ID from EMPLOYEES where SALARY >= " + input + "";
+
+        Connection conn = DBUtil.getConnection();
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(sql);
+            while (rs.next()) {
+                String name = rs.getString("last_name");
+                int salary = rs.getInt("salary");
+                Date hireDate = rs.getDate("hire_date");
+                int departmentID = rs.getInt("department_id");
+                System.out.println(name + "\t" + salary + "\t" + hireDate + "\t" + departmentID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (st != null) st.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        sc.close();
+    }
+}
